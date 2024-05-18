e1cbca411f8206e4a92f57d4f601171746af923c
SantiagoMena <santiagomenape@gmail.com>
2024-05-18 14:37:41 -0300

    List Users component

---
+++ src/components/Users.jsx
@@ -0,0 +1,39 @@
+'use client'
+import React, {useContext, useEffect, useState} from "react";
+
+import {CurrentUserContext} from "@/app/providers";
+import Header from "@/components/Header";
+import {getAllUsers} from "@/lib/firebase/firestore";
+import User from "@/components/User";
+
+export default function Users() {
+    const { currentUser, dispatchUser } = useContext(CurrentUserContext);
+    const [ users, setUsers ] = useState([]);
+
+    useEffect(() => {
+        async function getUsers() {
+            try {
+                const usersDB = await getAllUsers();
+                setUsers(usersDB);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        getUsers().catch(console.error);
+
+    }, []);
+
+    return (
+        <>
+            <Header />
+            <section className={"grid gap-6 lg:grid-cols-4 md:grid-cols-2 grid-cols-1"}>
+                {
+                    currentUser ?
+                        users && users.map((user) => <User key={user.uid} user={user} />)
+                        : <>LOGIN TO SEE THIS SECTION</>
+                }
+            </section>
+        </>
+    );
+}
