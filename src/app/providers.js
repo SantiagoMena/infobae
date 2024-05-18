53b5280b51833801cef3fe096535062af64be593
SantiagoMena <santiagomenape@gmail.com>
2024-05-18 14:07:03 -0300

    Persistir u obtener usuario en el localstorage

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,11 +1,31 @@
 'use client';
-import {createContext, useReducer} from "react";
+import {createContext, useEffect, useReducer} from "react";
 import {userReducer} from "@/app/reducers/userReducer";
 
 export const CurrentUserContext = createContext(undefined);
 
 export default function Providers({ children }) {
     const [currentUser, dispatchUser] = useReducer(userReducer, undefined);
+
+    // Default effect: obtener usuario de LocalStorage
+    useEffect(() => {
+        // Dispatch login si el el usuario existe en local storage
+        if (localStorage.getItem("user")) {
+            dispatchUser({
+                type: "LOGIN",
+                user: JSON.parse(localStorage.getItem("user")),
+            });
+        }
+    }, []);
+
+    useEffect(() => {
+        // Si el usuario cambio y no es nulo asignarlo al localstorage
+        if (currentUser) {
+            localStorage.setItem("user", JSON.stringify(currentUser));
+        } else { // Si el usuario es nulo o no se desasigno eliminar del local storage
+            localStorage.removeItem("user");
+        }
+    }, [currentUser]);
 
     return (
         <CurrentUserContext.Provider value={{
