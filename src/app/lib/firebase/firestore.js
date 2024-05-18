2d410f9d6b9daf017d21dd98ebce609c9333af33
SantiagoMena <santiagomenape@gmail.com>
2024-05-18 14:12:49 -0300

    Commentarios para los metodos de firestore

--- src/app/lib/firebase/firestore.js
+++ src/app/lib/firebase/firestore.js
@@ -1,17 +1,20 @@
 import {db} from "@/lib/firebase/clientApp";
-import { doc, setDoc, getDoc, getDocs, query, where, collection } from "firebase/firestore";
+import { doc, setDoc, getDoc, getDocs, query, collection } from "firebase/firestore";
 
+// persistir usuario en firestore
 export async function persistUser(user) {
     const userRef = collection(db, "users");
 
     return await setDoc(doc(userRef, String(user.uid)), {...user});
 }
 
+// Obtener todos los usuario existentes en la colección "users"
 export async function getAllUsers() {
     const q = query(collection(db, "users"));
     const querySnapshot = await getDocs(q);
     let users = [];
 
+    // Para los usuario obtenidos crear una array con los datos de cada uno
     querySnapshot.forEach((doc) => {
         users.push(doc.data());
     });
@@ -19,14 +22,18 @@ export async function getAllUsers() {
     return users;
 }
 
+// Obtener usuarios por su UID
 export async function getUserByUid(uid) {
     const userRef = doc(db, "users", uid);
     return await getDoc(userRef);
 }
 
+// Persistir usuario en firestore si no existe en la base de datos
 export async function persistUserIfNotExists(user) {
+    // Obtener el usuario de la base de datos
     let userDB = await getUserByUid(user.uid);
 
+    // Si no existe guardar el usuario
     if (!userDB.exists()) {
         userDB = await persistUser({
             uid: user.uid,
