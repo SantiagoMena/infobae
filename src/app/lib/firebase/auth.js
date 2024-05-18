2705d1750c06e2659d5460eef84e3700cb2b6033
SantiagoMena <santiagomenape@gmail.com>
2024-05-18 14:00:42 -0300

    Auth functions Firebase

---
+++ src/app/lib/firebase/auth.js
@@ -0,0 +1,26 @@
+import {
+    GoogleAuthProvider,
+    signInWithPopup,
+} from "firebase/auth";
+
+import { auth } from "@/lib/firebase/clientApp";
+
+// Función signIn Firebase Google Auth
+export async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+
+    try {
+        return await signInWithPopup(auth, provider);
+    } catch (error) {
+        console.error("Error signing in with Google", error);
+    }
+}
+
+// Función signOut Firebase Google Auth
+export async function signOut() {
+    try {
+        return auth.signOut();
+    } catch (error) {
+        console.error("Error signing out with Google", error);
+    }
+}
